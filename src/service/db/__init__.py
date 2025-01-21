ac57a5eb37fa010a2fc7b1cfd2f98e055443f98a
ToshibaMastr <ggostem42@gmail.com>
2025-01-21 03:53:25 +0300

    delete old get_db from db/__init__

--- src/service/db/__init__.py
+++ src/service/db/__init__.py
@@ -1,10 +1,9 @@
 from .base_class import SQLAlchemyBaseModel
 from .models import Endpoint, Server, ServerStatus, User
-from .session import get_db, init_db, async_session
+from .session import init_db, async_session
 
 __all__ = [
     "init_db",
-    "get_db",
     "async_session",
     "SQLAlchemyBaseModel",
     "Server",
